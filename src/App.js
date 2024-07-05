e2829ea4d3b3fbba313df5bec031334dd940fe29
Evilgozen <1814649459@qq.com>
2024-07-05 23:10:13 +0800

    put into ali

--- src/App.js
+++ src/App.js
@@ -1,127 +1,75 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Notification from './components/Notify'
-import Phone from './components/Phone'
-import phoneService from './services/phones'
+import noteService from './services/notes'
+import Note from './components/Note'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState('a new name...')
-  const [phones, setPhones] =useState([])
-  const [newNumber, setNewNumber] =useState('a new number...')
-  const [showPhones, setshowPhones] = useState([])
-  const [newFilter, setnewFilter] = useState('')
-  const [newWarning, setnewWarning] = useState(null)
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-  useEffect(()=> {
-    phoneService
+  useEffect(() => {
+    noteService
       .getAll()
-      .then(initialPhones => {
-        setPhones(initialPhones)
-        setshowPhones(initialPhones)
+      .then(response => {
+        setNotes(response)
       })
-  },[])
+  }, [])
 
-  useEffect(()=> {
-    if(newFilter==='') {
-      setshowPhones(phones)
-    }else {
-      setshowPhones(phones.filter(phone => {
-        return phone.name.toLowerCase().includes(newFilter)
-      }))
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
     }
-  },[newFilter,phones])
 
-
-  const handlefilter = (event) => {
-    if(event.target.value==='') {
-      setshowPhones(phones)
-    }
-    console.log(event.target.value)
-    setshowPhones(phones.filter(phone => {
-      return phone.name.toLowerCase().includes(event.target.value)
-    }))
-    setnewFilter(event.target.value)
-  }
-
-  const handledelete = phone => event =>  {
-    console.log(phone)
-    if(window.confirm("你确定要删除该电话记录吗?")) {
-      phoneService
-      .deletethis(phone)
-      .then(respon => {
-        const updatedPhones = phones.filter(p => p.id !== phone.id)
-        setPhones(updatedPhones)
-        setshowPhones(updatedPhones)
+    noteService
+      .create(noteObject)
+      .then(response => {
+        setNotes(notes.concat(response))
+        setNewNote('')
       })
-    }
-  } 
-
-  const addnewName = (event) => {
-      event.preventDefault()
-      const newObject = {
-        name:newName,
-        number:newNumber
-      }
-      
-      let same=false;
-      if(phones.some(e => e.name===newObject.name)) {
-        const thisId=phones.filter(e => e.name===newObject.name)[0].id
-        if(window.confirm("已经有该用户信息，是否更新用户信息?")) {
-          console.log("here:",thisId)
-          phoneService
-            .upgrade(thisId,newObject)
-            .then(response => {
-              const updatePhones = phones.map(p => p.id===thisId ? newObject : p)
-              setPhones(updatePhones)
-              setshowPhones(updatePhones)
-              setnewWarning(`${newObject.name}相关信息更新成功`)
-            })
-        }
-      }
-      else {
-        phoneService
-        .create(newObject)
-        .then(respon => {
-          setPhones(phones.concat(respon))
-          setNewName('')
-          setNewNumber('')
-          setnewWarning(`添加${newObject.name}相关信息成功`)
-        })
-      }
   }
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
   }
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
+  const toggleImportance = (id) => {
+    const url = `http://localhost:3001/api/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    noteService
+      .update(id,changedNote) 
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id != id ? note:returnedNote))
+      })
   }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Notification message={newWarning} />
-      <div>筛选:<input onChange={handlefilter} /></div>
-      <form onSubmit={addnewName}>
-        <div>
-          name: <input 
-                    value={newName}
-                    onChange={handleNameChange} />
-          number: <input
-                    value={newNumber}
-                    onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div> 
+      <ul>
+        <ul>
+          {notesToShow.map(note => 
+            <Note key={note.id} note={note} toggleImportance={()=>toggleImportance(note.id)}/>
+          )}
+        </ul>
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
       </form>
-      <h2>Numbers</h2>
-      <Phone phones={showPhones} handledelete={handledelete}/>
     </div>
   )
 }
